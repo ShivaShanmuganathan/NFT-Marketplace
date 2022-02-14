8c28456c4cc6a20ae3a117e33c984e0ca2cf93ab
Shiva <shanshiva@outlook.com>
2022-02-14 15:27:39 +0530

    Added Tests To Deploy ERC721 & NFT Market Contract

---
+++ test/market-test.js
@@ -0,0 +1,17 @@
+describe("NFTMarket", function() {
+  it("Should create and execute market sales", async function() {
+
+    const Market = await ethers.getContractFactory("NFTMarket")
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketAddress = market.address
+
+    const NFT = await ethers.getContractFactory("NFT")
+    const nft = await NFT.deploy(marketAddress)
+    await nft.deployed()
+    const nftContractAddress = nft.address
+
+    
+
+  })
+})
